fd998bf36912b711438ef36a499826a71be80ff1
Manish <manishday2010@gmail.com>
2022-11-22 23:25:57 +0530

    add build script

--- scripts/dockerBuild.sh
+++ scripts/dockerBuild.sh
@@ -1,4 +1,5 @@
 echo ${{ secrets.DOCKERHUB_PASSWORD }} | sudo docker login -u ${{ secrets.DOCKERHUB_USERNAME }} --password-stdin
+sudo docker build . -t python-app
 image_id=$(sudo docker images --filter "reference=python-app" --quiet)
 sudo docker tag $image_id manish3097/python-app
 sudo docker push manish3097/python-app
